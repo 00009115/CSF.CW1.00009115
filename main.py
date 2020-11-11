bceeb6adb5f747316b873e3f1788c659e0989351
00009115 <4BIS1.00009115@gmail.com>
2020-11-11 17:20:45 +0500

    Updating main.py after Directory Restructure

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from lists import transports
-from tuples import days
-from dictionaries import (capitals, continents)
+from lists.lists import transports
+from tuples.tuples import days
+from dictionaries.dictionaries import countries
 
 # We can always the access all types of data and use the items inside of them.
 # Python runs all statements of certain file while importing the modules from it which caused more output in this file,
 
-print("\nI want to travel France, which is located in " + continents["France"]["continent"])
-print("I am going to reach", capitals["France"]["capital"], "on", days[3], "on an", transports[0])
+print("\nI want to travel France, which is located in", countries["France"]["continent"])
+print("I am going to reach", countries["France"]["capital"], "on", days[3], "on an", transports[0])
