6e87a82a69da00f6f64bdc56b043e3a01ede3094
00009115 <4BIS1.00009115@gmail.com>
2020-11-11 17:01:47 +0500

    Updating the Dictionary Examples after Week 7 Seminar Topic

--- dictionaries.py
+++ dictionaries.py
@@ -1,60 +1,70 @@
 # Dictionary
 capitals = {
-    "Canada": {
-        "capital": "Ottawa",
-    },
-    "France": {
-        "capital": "Paris",
-    },
-    "Kazakhstan": {
-        "capital": "Astana",
-    }
+    "Canada":  "Ottawa",
+    "France": "Paris",
+    "Kazakhstan": "Astana",
+    "Russia": "Moscow"
 }
 
 continents = {
-    "Canada": {
-        "continent": "North America"
-    },
-    "France": {
-        "continent": "Europe"
-    },
-    "Kazakhstan": {
-        "continent": "Asia"
-    }
+    "Canada": "North America",
+    "France": "Europe",
+    "Kazakhstan": "Asia",
+    "Russia": ["Asia", "Europe"]  # ALl data types can contain another data type inside.
 }
 
-
 # Dictionaries contain much more complex data, namely keys and values which can be updated.
-# E.g. countries can have more specific information which have been showed using a dictionary above.
+capitals["Kazakhstan"] = "Nur-Sultan"
 
-# ALl data types can contain another data type which is shown in the dictionaries above. Each values can be edited.
-capitals["Kazakhstan"]["capital"] = "Nur-Sultan"
+# ZIPPING
 
 # Zipping the dictionaries directly is not a proper action as it zips only the key names but not the values.
-# Therefore we need to extract the items of dictionaries to lists using items() method.
+# zip(capitals, countries) would return [('Canada', 'Canada'), ('France', 'France'), ... ] which is not good.
+
+# What we can do is to extract the items of dictionaries to lists using items() method.
 capital_items = capitals.items()
 continent_items = continents.items()
 
-# Now we can use zip() function to merge these two lists.
-countries = list(zip(capital_items, continent_items))
+# Still zipping this two data is not a proper option. zip(capital_items, continent_items) returns the list below:
+# [(('Canada', 'Ottawa'), ('Canada', 'North America')), (('France', 'Paris'), ('France', 'Europe')), ... ))].
 
+# We can get much more comprehensive dictionary by looping this list.
+
+countries = dict()
+
+for (country, capital), (c, continent) in zip(capital_items, continent_items):
+    countries[country] = dict(capital=capitals[country], continent=continents[country])
+
+# Now countries is a dictionary received out of merging capitals and continents dictionaries comprehensively.
+# countries = {'Canada': {'capital': 'Ottawa', 'continent': 'North America'}, ... }
+
+# Using the data we reproduced above:
 print("\nZipping:")
 
 for country in countries:
-    country_name = country[0][0]
-    continent = country[1][1].get("continent")
-    capital = country[0][1].get("capital")
-    print(country_name, "is located in", continent + ". Its capital is", capital + ".")
+    if country == "Russia":
+        print(country, "is located in", countries[country]["continent"][0], "and", countries[country]["continent"][1],
+              "and its capital is", countries[country]["capital"])
+    else:
+        print(country, "is located in", countries[country]["continent"],
+              "and its capital is", countries[country]["capital"])
 
+# ENUMERATING
 
 # Enumerating the dictionaries directly are not the best solution, because it keeps and numerates only the keys.
-# To enumerate the dictionaries items keeping the value as well, we should separate the items out of the dictionary.
-ordered_countries = list(enumerate(capitals.items(), 1))  # Second parameter sets the starting point of enumerating.
+# enumerate(countries) returns [(0, 'Canada'), (1, 'France'), (2, 'Kazakhstan'), (3, 'Russia')]
+# To keep the values while enumerating, we can extract the items out of the dictionary into the list.
+numbered_countries = list(enumerate(countries.items(), 1))  # Second parameter sets the starting point of enumerating.
 
+# Using the data we reproduced above
 print("\nEnumerating:")
 
-for country in ordered_countries:
-    country_name = country[1][0]
-    capital = country[1][1].get("capital")
-    print(str(country[0]) + ")", country_name, "-", capital)
-
+for country in numbered_countries:   # = (1, ('Canada', {'capital': 'Ottawa', 'continent': 'North America'})) etc.
+    country_info = country[1]        # = ('Canada', {'capital': 'Ottawa', 'continent': 'North America'}) etc.
+    country_name = country_info[0]   # = 'Canada' etc.
+    capital = country_info[1]["capital"]      # = 'Ottawa' etc.
+    continent = country_info[1]["continent"]  # = 'North America' etc.
+    if country_name == "Russia":
+        print(str(country[0]) + ")", capital + ",", country_info[0] + ",", continent[0], "/", continent[1])
+    else:
+        print(str(country[0]) + ")", capital + ",", country_info[0] + ",", continent)
