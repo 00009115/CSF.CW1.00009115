2720dce9704f441840e1212c134d1fad6ea06e90
00009115 <4bis1.00009115@gmail.com>
2021-01-14 17:15:00 +0500

    Task 8

---
+++ FINAL EXAM SOLUTIONS/Python Tasks/Task-8.py
@@ -0,0 +1,36 @@
+# VOID FUNCTIONS
+# void functions do not return any value after being executed
+
+message = ''
+
+
+def check_age_void(age):
+    global message
+
+    if age < 18:
+        message = "You are " + str(age) + " years old, thus not eligible for the installment."
+    elif age >= 18:
+        message = "You are " + str(age) + " years old and eligible to receive an installment."
+
+
+# Now the function above should be executed separately in order to update the message to be printed.
+
+check_age_void(16)
+print(message)
+
+# To avoid that, we can use value-returning functions which are shown below:
+
+
+# VALUE RETURNING FUNCTIONS
+
+def check_age_value(age):
+    if age < 18:
+        return "You are " + str(age) + " years old, thus not eligible for the installment."
+    elif age >= 18:
+        return "You are " + str(age) + " years old and eligible to receive an installment."
+
+# Now we can use the function as an argument of print() function as it returns string value
+
+
+print(check_age_value(20))
+
